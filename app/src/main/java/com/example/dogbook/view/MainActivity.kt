0000b3a06e97030be8c979b925a13dd02a428528
gabona07 <bonagabriella1995@gmail.com>
2020-10-06 15:27:58 +0200

    Create basic layout

--- app/src/main/java/com/example/dogbook/view/MainActivity.kt
+++ app/src/main/java/com/example/dogbook/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dogbook
+package com.example.dogbook.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dogbook.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
