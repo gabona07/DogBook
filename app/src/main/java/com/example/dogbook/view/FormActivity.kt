f6bdfb6e3ff2218092b91cf37a1c52d614ef6baa
gabona07 <bonagabriella1995@gmail.com>
2020-10-12 19:13:36 +0200

    Create main layout and fab

---
+++ app/src/main/java/com/example/dogbook/view/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dogbook.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dogbook.R
+
+class FormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
--- app/src/main/java/com/example/dogbook/view/MainPageActivity.kt
+++ app/src/main/java/com/example/dogbook/view/MainPageActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dogbook.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.dogbook.R
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+    }
+
+    fun navigateToForm(view: View) {
+        val intent = Intent(this, FormActivity::class.java)
+        startActivity(intent)
     }
 }
